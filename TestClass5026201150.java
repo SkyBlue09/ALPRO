57edf0128bf19b9dd02dae5f206257d2d6a290ba
SkyBlue09 <81377154+SkyBlue09@users.noreply.github.com>
2021-04-01 22:29:06 +0800

    Add files via upload

---
+++ TestClass5026201150.java
@@ -0,0 +1,9 @@
+public class TestClass5026201150{
+
+	public static void main(String[] args){
+	ComputeMethods5026201150 ComputeMethods5026201150 = new ComputeMethods5026201150();
+	System.out.println("Temp in celcius is " + ComputeMethods5026201150.fToC(100.4));
+	System.out.println("Hypotenuse is " + ComputeMethods5026201150.hypotenuse(9, 6));
+	System.out.println("The sum of the dice values is " + ComputeMethods5026201150.roll());
+	}
+}
